5042e962edf3530a67cfd7278f6b73901e9fa497
guddetisyamprasad <83542655+guddetisyamprasad@users.noreply.github.com>
2023-05-23 14:34:54 +0530

    fetching name by  23.05.2023

--- RestApI/src/main/java/com/Employee/Repository/EmployeeRepository.java
+++ RestApI/src/main/java/com/Employee/Repository/EmployeeRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
-	
+
 	@Query(value="select e.number,e.first_name,e.last_name,e.email,e.address_id,e.salary from emp e where e.number=:id and e.first_name=:name",nativeQuery = true)
 	public Optional<Employee> findByIdAndName(int id,String name);
 
 	@Query(value="select e.number,e.first_name,e.last_name,e.email,e.address_id,e.salary from emp e where e.first_name=:name",nativeQuery = true)
 	public Optional<Employee>  findByFirstName(String name);
+
+	@Query(value="select e.first_name,e.last_name from emp e order by first_name",nativeQuery = true)
+	public List<Object> getByName();
 }
